c32d21e0893ca47a216b5624357813a29e9339d9
FrankD-BYUI <fdesjardins@byui.edu>
2019-10-12 17:34:03 -0700

    Added lesson 4 folder and content

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,9 @@ let currentYear = now.getFullYear();
 document.getElementById('currentYear').innerHTML = currentYear;
 
 let d = new Date(document.lastModified);
-let lastUpdate = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+
+let sec = '00' + d.getSeconds();
+let min = '00' + d.getMinutes();
+
+let lastUpdate = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + min.substr(min.length-2) + ':' + sec.substr(sec.length-2);
 document.getElementById('lastUpdate').innerHTML = lastUpdate;
---
+++ lesson4/js/scripts.js
@@ -0,0 +1,16 @@
+let now = new Date();
+let currentYear = now.getFullYear();
+document.getElementById('currentYear').innerHTML = currentYear;
+
+let d = new Date(document.lastModified);
+
+let sec = '00' + d.getSeconds();
+let min = '00' + d.getMinutes();
+
+let lastUpdate = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + min.substr(min.length - 2) + ':' + sec.substr(sec.length - 2);
+document.getElementById('lastUpdate').innerHTML = lastUpdate;
+
+
+function toggleMenu() {
+  document.getElementById("nav-menu").classList.toggle("show-menu")
+}
