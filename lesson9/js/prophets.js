906c0cdd3177636344c28a4b5c3cd502ec7f38be
FrankD-BYUI <fdesjardins@byui.edu>
2019-11-15 20:57:23 -0700

    Prophets page finished.

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -9,10 +9,22 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
+      let bDate = document.createElement('p');
+      let bPlace = document.createElement('p');
+      let portrait = document.createElement('img')
 
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
       card.appendChild(h2);
+
+      bDate.textContent = 'Date of birth: ' + prophets[i].birthdate;
+      card.appendChild(bDate);
+
+      bPlace.textContent = 'Place of birth: ' + prophets[i].birthplace;
+      card.appendChild(bPlace);
+
+      portrait.setAttribute('src', prophets[i].imageurl);
+      portrait.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+      card.appendChild(portrait);
 
       document.querySelector('div.cards').appendChild(card);
     }
