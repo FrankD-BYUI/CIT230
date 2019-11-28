f860a8660e8085be10a4f6012b88385e39f82dfa
FrankD-BYUI <fdesjardins@byui.edu>
2019-11-27 17:34:30 -0700

    Added week 11 folder

---
+++ lesson11/js/weather.js
@@ -0,0 +1,64 @@
+/********
+ * This code uses the OpenWeatherMap API to pull 
+ * real time weather data for use on the town pages
+ ********/
+const apiURLCurrent = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c03799aebfe07c839f8755c32064fd53'
+fetch(apiURLCurrent)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    document.getElementById('current-condition').textContent = jsObject.weather[0].description;
+    document.getElementById('high-temp').textContent = jsObject.main.temp_max;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('wind-speed').textContent = jsObject.wind.speed;
+  });
+
+/********
+ * This code uses the OpenWeatherMap API to pull 
+ * weather forcast data for use on the town pages
+ ********/
+const apiURLForecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=c03799aebfe07c839f8755c32064fd53'
+fetch(apiURLForecast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    //const weatherList = jsonObject.list;
+
+    let day = 1;
+
+    for (let i = 0; i < jsObject.list.length; i++) {
+      if (jsObject.list[i].dt_txt.includes("18:00:00")) {
+        //Set the day
+        let dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+        let date = new Date(jsObject.list[i].dt_txt);
+        document.getElementById('day' + day + '-day').textContent = dayNames[date.getDay()];
+
+        //set the icon
+        let iconSrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
+        let iconAlt = jsObject.list[i].weather[0].description;
+        document.getElementById('day' + day + '-icon').setAttribute('src', iconSrc);
+        document.getElementById('day' + day + '-icon').setAttribute('alt', iconAlt);
+
+
+        //set the temp
+        document.getElementById('day' + day + '-temp').textContent = jsObject.list[i].main.temp.toFixed(0);
+
+
+        day++;
+      }
+    }
+  });
+
+
+
+/********
+ * This code calculates windchill for use on the town pages
+ ********/
+var temp = parseFloat(document.getElementById("high-temp").textContent);
+var windSpeed = parseFloat(document.getElementById('wind-speed').textContent);
+var windChill = 0
+
+if (temp <= 50 && windSpeed >= 3) {
+  windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+  document.getElementById('wind-chill').textContent = Math.round(windChill);
+}
